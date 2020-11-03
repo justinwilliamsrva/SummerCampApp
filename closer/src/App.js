211fbf8db4ff40050a29d448dca60239f7083ee8
justinwilliamsrva <justin.williams2@me.com>
2020-11-03 15:29:45 -0500

    created buttons for delete and update

--- closer/src/App.js
+++ closer/src/App.js
@@ -3,8 +3,6 @@ import Nav from "./Nav";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-
-
 export default function App() {
     const [posts, setPosts] = useState([]);
 
@@ -29,14 +27,28 @@ export default function App() {
             {posts.map((post, i) => (
                 <div className="row" key={post._id}>
                     <div className="col pt-3 pb-2">
-                        <Link to={`/post/${post.slug}`}><h2>{post.title}</h2></Link>
-                        <p className="lead">{post.content.substring(0,10)}</p>
-                        <p>
-                            Author<span className="badge">{post.user}</span> Published on{""}
-                            <span className="badge">
-                                {new Date(post.createdAt).toLocaleString()}
-                            </span>
-                        </p>
+                        <div className="row">
+                            <div className="col-md-10">
+                                <Link to={`/post/${post.slug}`}>
+                                    <h2>{post.title}</h2>
+                                </Link>
+                                <p className="lead">{post.content.substring(0, 10)}</p>
+                                <p>
+                                    Author<span className="badge">{post.user}</span> Published on
+                                    {""}
+                                    <span className="badge">
+                                        {new Date(post.createdAt).toLocaleString()}
+                                    </span>
+                                </p>
+                            </div>
+
+                            <div className="col-md-2">
+                      <Link to={`/post/update/${post.slug}`} className="btn btn-sm btn-outline-warning mr-1">
+                                    Update
+                      </Link>
+                      <button className="btn btn-sm btn-outline-danger">Delete</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             ))}
