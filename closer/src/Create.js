9911c1b67041c0bb329c0bb447b6a5aee0b90cb3
justinwilliamsrva <justin.williams2@me.com>
2020-11-03 10:36:41 -0500

    Fixes needed

--- closer/src/Create.js
+++ closer/src/Create.js
@@ -1,55 +1,72 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-export default function Create() {
+const Create = () => {
+    // state
     const [state, setState] = useState({
-        // state
-        title: "",
-        content: "",
-        user: "",
+        title: '',
+        content: '',
+        user: ''
     });
-
+    // destructure values from state
     const { title, content, user } = state;
 
-    function handleChange(name) {
-        return function (event) {
-            // console.log("name", name, "event", event.target.value)
-            setState({ ...state, [name]: event.target.value })
+    // onchange event handler
+    const handleChange = name => event => {
+        // console.log('name', name, 'event', event.target.value);
+        setState({ ...state, [name]: event.target.value });
+    };
 
-
-        }
-    }
+    const handleSubmit = event => {
+        event.preventDefault();
+        // console.table({ title, content, user });
+        axios
+            .post(`${process.env.REACT_APP_API}/post`, { title, content, user })
+            .then(response => {
+                console.log(response);
+                // empty state
+                setState({ ...state, title: '', content: '', user: '' });
+                // show sucess alert
+                alert(`Post titled ${response.data.title} is created`);
+            })
+            .catch(error => {
+                console.log(error.response);
+                alert(error.response.data.error);
+            });
+    };
 
     return (
         <div className="container p-5">
             <h1>CREATE POST</h1>
             <br />
-            <form>
+
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label className="text-muted">Title</label>
                     <input
-                        onChange={handleChange("title")}
+                        onChange={handleChange('title')}
                         value={title}
                         type="text"
                         className="form-control"
-                        placeholder="Post Title"
+                        placeholder="Post title"
                         required
                     />
                 </div>
                 <div className="form-group">
                     <label className="text-muted">Content</label>
                     <textarea
-                        onChange={handleChange("content")}
+                        onChange={handleChange('content')}
                         value={content}
                         type="text"
                         className="form-control"
-                        placeholder="Write Something.."
+                        placeholder="Write something.."
                         required
                     />
                 </div>
                 <div className="form-group">
                     <label className="text-muted">User</label>
                     <input
-                        onChange={handleChange("user")}
+                        onChange={handleChange('user')}
                         value={user}
                         type="text"
                         className="form-control"
@@ -63,4 +80,6 @@ export default function Create() {
             </form>
         </div>
     );
-}
+};
+
+export default Create;
