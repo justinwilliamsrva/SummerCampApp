6b7d7256578eb78e08ee5555484176548ff50745
justinwilliamsrva <justin.williams2@me.com>
2020-11-02 20:01:16 -0500

    connected mongodb

--- server/server.js
+++ server/server.js
@@ -1,31 +1,38 @@
-const express = require("express")
-const morgan = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const mongoose = require("mongoose")
-require("dotenv").config()
-
+const express = require("express");
+const morgan = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // app
-const app = express()
+const app = express();
 
+// db
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+
+    })
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log(err));
 
 // middlewares
-app.use(cors())
-app.use(morgan('dev'))
-app.use(bodyParser.json())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
 
 // route
 app.get("*", (req, res) => {
     res.json({
-        data: "You reached node.js api for reach node crud app"
-
-    })
+        data: "You reached node.js api for reach node crud app",
+    });
 });
 
-
-
 // port
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.listen(port,() => console.log(`server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
