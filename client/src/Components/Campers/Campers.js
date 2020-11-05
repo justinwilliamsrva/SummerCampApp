fb4d120077a76fa3daa38dfecfa3ac129846391a
justinwilliamsrva <justin.williams2@me.com>
2020-11-05 11:55:58 -0500

    create files for Cmaper components

---
+++ client/src/Components/Campers/Campers.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import Nav from "../../Nav";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { getUser, getToken } from "../../helpers";
+export default function Supplies() {
+    const [items, setitems] = useState([]);
+
+    const fetchitems = () => {
+        axios
+            .get(`${process.env.REACT_APP_EQUIPT_API}/equiptment`)
+            .then((response) => {
+                console.log(response, setitems(response.data));
+            })
+            .catch((error) => alert(error));
+    };
+
+    useEffect(() => {
+        fetchitems();
+    }, []);
+
+
+    return (
+        <div className="container pb-5">
+            <Nav />
+            <h1>All Equiptment</h1>
+
+            {getUser() && (
+                <Link to={`equiptment/create`} className="btn btn-lg btn-primary">
+                    Create a Item
+                </Link>
+            )}
+            <br />
+            <table class="table table-striped mt-1 table-responsive ">
+                <thead>
+                    <tr>
+                        <th scope="col">Item</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Available</th>
+                        <th scope="col">Last User</th>
+                        <th scope="col">Notes</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {items.map((item, i) => (
+
+                        <tr>
+                            <Link to={`/equiptment/update/${item.slug}`}>
+                                <th key={item._id}>{item.item}</th>
+                                </Link>
+                            <td>{item.location}</td>
+                            {item.availability ? <td>In Stock</td> : <td>Being Used</td>}
+                            <td>{item.user}</td>
+                            <td>{item.notes}</td>
+                            </tr>
+
+                    ))}
+
+                </tbody>
+            </table>
+        </div>
+    );
+}
---
+++ client/src/Components/Campers/UpdateSupplies.js
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Nav from "../../Nav";
+import { getUser, getToken } from "../../helpers";
+import { useHistory } from "react-router-dom";
+export default function UpdatePost(props) {
+    const [state, setState] = useState({
+        item: "",
+        location: "",
+        slug: "",
+        user: "",
+        availability: "",
+        notes: "",
+    })
+
+    const { item, location, slug, user, availability, notes } = state;
+    useEffect(() => {
+        axios
+            .get(`${process.env.REACT_APP_EQUIPT_API}/equiptment/${props.match.params.slug}`)
+            .then((response) => {
+                console.log(response);
+                const { item, location, slug, user, availability, notes } = response.data;
+                setState({ ...state, item, location, slug, user, availability, notes });
+            })
+            .catch((error) => alert("Error loading single post"));
+    }, []);
+    const handleChange = (name) => (event) => {
+        // console.log('name', name, 'event', event.target.value);
+        setState({ ...state, [name]: event.target.value });
+    };
+    let history = useHistory();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // console.table({ title, content, user });
+        axios
+            .put(`${process.env.REACT_APP_EQUIPT_API}/equiptment/${slug}`, {
+                item,
+                location,
+                slug,
+                user,
+                availability,
+                notes,
+            })
+            .then((response) => {
+                console.log(response);
+                const { item, location, slug, user, availability, notes } = response.data;
+
+                setState({ ...state, item, location, slug, user, availability, notes });
+                // show sucess alert
+                alert(`The item '${item}' is updated`);
+                history.push("/equiptment");
+            })
+            .catch((error) => {
+                console.log(error.response);
+                alert("error");
+            });
+    };
+    const deleteConfirm = (slug) => {
+        let answer = window.confirm("Are you sure you want to delete this item?");
+
+        if (answer) {
+            deletePost(slug);
+        }
+    };
+
+
+    const deletePost = (slug) => {
+
+        axios
+            .delete(`${process.env.REACT_APP_EQUIPT_API}/equiptment/${slug}`, {
+                headers: { authorization: `Bearer ${getToken()}` },
+            })
+            .then((response) => {
+                console.log(response);
+                alert(response.data.message);
+                history.push("/equiptment");
+
+            })
+            .catch((error) => alert(error));
+    };
+
+
+    const showUpdateForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label className="text-muted">Location</label>
+                <input
+                    onChange={handleChange("location")}
+                    value={location}
+                    type="text"
+                    className="form-control"
+                    required
+                />
+            </div>
+
+            <label className="text-muted">Availability</label>
+            <div class="custom-control custom-radio">
+                <input
+                    type="radio"
+                    id="customRadio1"
+                    name="customRadio"
+                    class="custom-control-input"
+                />
+                <label class="custom-control-label" for="customRadio1">
+                    In stock
+                </label>
+            </div>
+            <div class="custom-control custom-radio">
+                <input
+                    type="radio"
+                    id="customRadio2"
+                    name="customRadio"
+                    class="custom-control-input"
+                />
+                <label class="custom-control-label" for="customRadio2">
+                    Being Used
+                </label>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">User</label>
+                <input
+                    onChange={handleChange("user")}
+                    value={user}
+                    type="text"
+                    className="form-control"
+                    required
+                />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Notes</label>
+                <input
+                    onChange={handleChange("notes")}
+                    value={notes}
+                    type="text"
+                    className="form-control"
+                    required
+                />
+            </div>
+            <div>
+                <button className="btn btn-primary">Update</button>
+
+            </div>
+        </form>
+    );
+
+    return (
+        <div className="container pb-5">
+            <Nav />
+            <br />
+            <h1>
+                Update <span style={{ color: "blue" }}>{item}</span> Information
+            </h1>
+            {showUpdateForm()}
+            {getUser() && (
+                <button
+                    onClick={() => deleteConfirm(slug)}
+                    className="btn btn-danger px-3">
+                    Delete
+                </button>
+            )}
+
+        </div>
+    );
+}
