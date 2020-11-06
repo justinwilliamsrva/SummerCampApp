903709697c714ec3ee0221fb10d594c0d9728f1a
justinwilliamsrva <justin.williams2@me.com>
2020-11-06 10:19:28 -0500

    fixed heading break

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -7,7 +7,7 @@ const Nav = ({ history }) => {
     return (
         <nav id="nav"className="nav nav-tabs">
             <img src="/fire1.png" width="50" height="50" />
-            <h3>Camp Fire</h3>
+            <h3>Camp Fire<span id="heading"> Staff App</span></h3>
 
             <ul className="nav ml-auto">
                 {!getUser() && (
