ddabf2e09dc78687c5ae99d49b2988e5f17bf62e
justinwilliamsrva <justin.williams2@me.com>
2020-11-07 08:48:16 -0500

    new more icons

--- client/src/Components/More.js
+++ client/src/Components/More.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Nav from '../Nav'
-import Footer from "../Components/Global/Footer"
-import "../Components/Messages/App.css"
+import React from "react";
+import Nav from "../Nav";
+import Footer from "../Components/Global/Footer";
+import "../Components/Messages/App.css";
 
 export default function More() {
     return (
@@ -9,21 +9,28 @@ export default function More() {
             <Nav />
             <section>
                 <div id="app-header">
-
                     <h1>More Options</h1>
                 </div>
                 <hr />
-                <br/>
+                <br />
 
-                <ul class="fa-ul">
-  <li><h2>Calendar</h2></li>
-  <li><h2>Contacts</h2></li>
-  <li><h2>Room Sign Out</h2></li>
-  <li><h2>Arrivals and Departures</h2></li>
-</ul>
+                <ul style={{listStyleType:"none"}}>
+                    <li>
 
+                        <h2><img src="/calendar.png" /> Calendar</h2>
+                    </li>
+                    <li>
+                        <h2><img src="/phone-book.png" /> Contacts</h2>
+                    </li>
+                    <li>
+                        <h2><img src="/door.png" /> Room Sign Out</h2>
+                    </li>
+                    <li>
+                        <h2><img src="/airplane.png" /> Arrivals and Departures</h2>
+                    </li>
+                </ul>
             </section>
             <Footer />
         </div>
-    )
+    );
 }
