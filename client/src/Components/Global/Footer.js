920b6d133a5f24208fda1915a69ad763e0e9dc9d
justinwilliamsrva <justin.williams2@me.com>
2020-11-06 21:05:37 -0500

    fixed footer image rendering

--- client/src/Components/Global/Footer.js
+++ client/src/Components/Global/Footer.js
@@ -7,10 +7,10 @@ const Footer = ({ history }) => {
         <footer id="footer"className="d-flex justify-content-between" >
             <ul id="footerList" className="nav mx-4 py-1 py-md-2 py-lg-2 d-flex justify-content-between">
                 <li className="pt-1 pt-lg-0 pb-2">
-                    <Link to="/campers"><img src="boy.png"/></Link>
+                    <Link to="/campers"><img src="/boy.png"/></Link>
                 </li>
                 <li className="pt-2 pt-lg-0 pb-2">
-                    <Link to="/"><img src="message.png"/></Link>
+                    <Link to="/"><img src="/message.png"/></Link>
                 </li>
                 <li className="pt-1 pt-lg-0 pb-2">
                     <Link to="/equiptment"><img src="/petition.png"/></Link>
@@ -18,6 +18,7 @@ const Footer = ({ history }) => {
                 <li className="pt-1 pt-lg-0 pb-2">
                     <Link to="/more"><img src="/more.png"/></Link>
                 </li>
+                
             </ul>
         </footer>
     );
