e6e5c17cd9c36485a524762024c310c923942e23
justinwilliamsrva <justin.williams2@me.com>
2020-11-07 04:44:50 -0500

    add More file and new font

--- client/src/Components/More.js
+++ client/src/Components/More.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Nav from '../Nav'
+import Footer from "../Components/Global/Footer"
+import "../Components/Messages/App.css"
 
 export default function More() {
     return (
         <div>
-            
+            <Nav />
+            <section>
+                <div id="app-header">
+
+                    <h1>More Options</h1>
+                </div>
+                <hr />
+                <br/>
+
+                <ul class="fa-ul">
+  <li><h2>Calendar</h2></li>
+  <li><h2>Contacts</h2></li>
+  <li><h2>Room Sign Out</h2></li>
+  <li><h2>Arrivals and Departures</h2></li>
+</ul>
+
+            </section>
+            <Footer />
         </div>
     )
 }
